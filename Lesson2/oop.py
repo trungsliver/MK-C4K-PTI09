6142c1bf189a8653d1883804929e11749ea98f1a
trungsliver <buiductrung.cva@gmail.com>
2025-02-16 09:46:03 +0700

    Lesson 2

---
+++ Lesson2/oop.py
@@ -0,0 +1,13 @@
+# Bài 1: Xây dựng class Rectangle gồm:
+# 	- Thuộc tính: width, height
+# 	- Phương thức:
+# 		+ cvi(): tính chu vi HCN
+# 		+ dtich(: tính diện tích HCN
+
+# Bài 2: Xây dựng class BankAccount gồm:
+# 	- Thuộc tính: 
+# 		+ acc_number (số tài khoản)
+# 		+ balance (số dư)
+# 	- Phương thức:
+# 		+ deposit(): nạp tiền vào tài khoản
+# 		+ withdraw(): rút tiền và cập nhật số dư
